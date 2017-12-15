a15ce0a55b227673766fe3cd9155fe4dfdc84c6a
Sweeper777 <sumulang@gmail.com>
2017-12-15 07:12:07 +0000

    added init for RectArranger

--- AutoArrange/RectArranger.swift
+++ AutoArrange/RectArranger.swift
@@ -5,4 +5,9 @@ class RectArranger {
     var arrangedRects = RectCollection(rects: [])
     let idealRatio: Double
     
+    init(rects: [CGRect], idealRatio: Double = 1 / sqrt(2)) {
+        self.rectsToBeArranged = rects
+        self.idealRatio = idealRatio
+    }
+    
 }
