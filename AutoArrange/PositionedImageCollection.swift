477f6c4e17f0bc6cb64595048b2db592e2c9dd3c
Sweeper777 <sumulang@gmail.com>
2017-12-24 08:18:14 +0800

    changed RectCollection to PositionedImageCollection

--- AutoArrange/PositionedImageCollection.swift
+++ AutoArrange/PositionedImageCollection.swift
@@ -1,58 +1,60 @@
 import CoreGraphics
 
-struct RectCollection: Sequence {
-    typealias Iterator = Array<CGRect>.Iterator
-    typealias SubSequence = Array<CGRect>.SubSequence
+struct PositionedImageCollection: Sequence, Codable {
+    typealias Iterator = Array<PositionedImage>.Iterator
+    typealias SubSequence = Array<PositionedImage>.SubSequence
     
     public func makeIterator() -> Iterator {
-        return rects.makeIterator()
+        return images.makeIterator()
     }
     
     public var underestimatedCount: Int {
-        return rects.underestimatedCount
+        return images.underestimatedCount
     }
     
     public func map<T>(_ transform: (Iterator.Element) throws -> T) rethrows -> [T] {
-        return try rects.map(transform)
+        return try images.map(transform)
     }
     public func filter(_ isIncluded: (Iterator.Element) throws -> Bool) rethrows -> [Iterator.Element] {
-        return try rects.filter(isIncluded)
+        return try images.filter(isIncluded)
     }
     
     public func forEach(_ body: (Iterator.Element) throws -> Swift.Void) rethrows {
-        try rects.forEach(body)
+        try images.forEach(body)
     }
     public func dropFirst(_ n: Int) -> SubSequence {
-        return rects.dropFirst(n)
+        return images.dropFirst(n)
     }
     public func dropLast(_ n: Int) -> SubSequence {
-        return rects.dropLast(n)
+        return images.dropLast(n)
     }
     
     public func drop(while predicate: (Iterator.Element) throws -> Bool) rethrows -> SubSequence {
-        return try rects.drop(while: predicate)
+        return try images.drop(while: predicate)
     }
     
     public func prefix(_ maxLength: Int) -> SubSequence {
-        return rects.prefix(maxLength)
+        return images.prefix(maxLength)
     }
     
     public func prefix(while predicate: (Iterator.Element) throws -> Bool) rethrows -> SubSequence {
-        return try rects.prefix(while: predicate)
+        return try images.prefix(while: predicate)
     }
     
     public func suffix(_ maxLength: Int) -> SubSequence {
-        return rects.suffix(maxLength)
+        return images.suffix(maxLength)
     }
     
     public func split(maxSplits: Int, omittingEmptySubsequences: Bool, whereSeparator isSeparator: (Iterator.Element) throws -> Bool) rethrows -> [SubSequence] {
-        return try self.rects.split(maxSplits: maxSplits, omittingEmptySubsequences: omittingEmptySubsequences, whereSeparator: isSeparator)
+        return try self.images.split(maxSplits: maxSplits, omittingEmptySubsequences: omittingEmptySubsequences, whereSeparator: isSeparator)
     }
     
-    var rects: [CGRect] = []
+    var images: [PositionedImage] = []
     
-    init(rects: [CGRect]) {
-        self.rects = rects
+    init(images: [PositionedImage]) {
+        self.images = images
     }
+    
+    
 }
 
