0bdd591e19ae43c9ddfa7e464bd7bea163298539
Sweeper777 <sumulang@gmail.com>
2017-12-24 08:20:13 +0800

    added decode and encode for scaleFactor

--- AutoArrange/PositionedImage.swift
+++ AutoArrange/PositionedImage.swift
@@ -15,16 +15,19 @@ struct PositionedImage: Codable {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         image = try UIImage(data: container.decode(Data.self, forKey: .image))!
         rect = try container.decode(CGRect.self, forKey: .rect)
+        scaleFactor = try container.decode(Double.self, forKey: .scaleFactor)
     }
     
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(UIImagePNGRepresentation(image), forKey: .image)
         try container.encode(rect, forKey: .rect)
+        try container.encode(scaleFactor, forKey: .scaleFactor) 
     }
     
     enum CodingKeys: CodingKey {
         case image
         case rect
+        case scaleFactor
     }
 }
