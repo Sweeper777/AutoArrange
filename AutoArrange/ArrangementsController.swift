2480d129c1c6ac2ec63d8b8681a608f54d468a7f
Sweeper777 <sumulang@gmail.com>
2017-12-25 08:38:26 +0800

    added delegate methods

--- AutoArrange/ArrangementsController.swift
+++ AutoArrange/ArrangementsController.swift
@@ -2,5 +2,12 @@ import UIKit
 import TLPhotoPicker
 
 class ArrangementsController: UITableViewController, TLPhotosPickerViewControllerDelegate {
+    func dismissPhotoPicker(withTLPHAssets: [TLPHAsset]) {
+        
+    }
+    
+    func dismissComplete() {
+        
+    }
 }
 
