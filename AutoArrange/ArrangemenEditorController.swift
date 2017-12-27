8df45fc975db67739af3e36690f83a55c55f7955
Sweeper777 <sumulang@gmail.com>
2017-12-27 08:18:33 +0800

    added cancel and arrange buttons

--- AutoArrange/ArrangemenEditorController.swift
+++ AutoArrange/ArrangemenEditorController.swift
@@ -3,4 +3,8 @@ import Eureka
 
 class ArrangementEditorController : FormViewController {
     var images: [UIImage]!
+    
+    @IBAction func cancel() {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
