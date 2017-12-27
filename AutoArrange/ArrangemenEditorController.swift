35a2f892c0befe00845486618dc8942f3f55cf71
Sweeper777 <sumulang@gmail.com>
2017-12-27 08:31:43 +0800

    added name row

--- AutoArrange/ArrangemenEditorController.swift
+++ AutoArrange/ArrangemenEditorController.swift
@@ -4,6 +4,17 @@ import Eureka
 class ArrangementEditorController : FormViewController {
     var images: [UIImage]!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        form +++ Section()
+        <<< TextRow(tagName) {
+            row in
+            row.title = NSLocalizedString("Name", comment: "")
+            row.placeholder = NSLocalizedString("Unnamed", comment: "")
+        }
+    }
+    
     @IBAction func cancel() {
         self.dismiss(animated: true, completion: nil)
     }
