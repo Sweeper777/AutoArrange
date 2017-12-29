415cd2bd9a3192595299a983ad1203376aed851f
Sweeper777 <sumulang@gmail.com>
2017-12-29 08:29:05 +0800

    added conversion from WHRatios to Double ratio

--- AutoArrange/WHRatios.swift
+++ AutoArrange/WHRatios.swift
@@ -1,4 +1,15 @@
+import Darwin
+
 enum WHRatios : String {
     case landscape = "Landscape"
     case portrait = "Portrait"
+    
+    var ratio: Double {
+        switch self {
+        case .landscape:
+            return sqrt(2)
+        case .portrait:
+            return 1 / sqrt(2)
+        }
+    }
 }
