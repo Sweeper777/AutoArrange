d3916ad967f127405149196db324f8a22b8a530a
Sweeper777 <sumulang@gmail.com>
2017-12-30 10:06:41 +0800

    updated the Arrangement class again

--- AutoArrange/Arrangement.swift
+++ AutoArrange/Arrangement.swift
@@ -1,7 +1,8 @@
 import RealmSwift
 
 class Arrangement: Object {
+    @objc dynamic var name = NSLocalizedString("Unnamed ", comment: "")
     @objc dynamic var imagesData: Data?
-    @objc dynamic var margin = 7
+    @objc dynamic var margin = 7.0
     @objc dynamic var ratio = 1 / sqrt(2)
 }
