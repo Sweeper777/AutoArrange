494ee8d8d8c75d37df12a529c00fb238c538525f
Sweeper777 <sumulang@gmail.com>
2018-01-19 14:00:24 +0000

    calculated image rect

--- AutoArrange/ArrangementPreviewController.swift
+++ AutoArrange/ArrangementPreviewController.swift
@@ -8,5 +8,13 @@ class ArrangementPreviewController: UIViewController {
     override func viewDidLoad() {
         let decoder = PropertyListDecoder()
         let imageCollection = try! decoder.decode(PositionedImageCollection.self, from: arrangement.imagesData!)
+        for positionedImage in imageCollection {
+            let imageRect = CGRect(
+                x: positionedImage.rect.x + arrangement.margin.f,
+                y: positionedImage.rect.y + arrangement.margin.f,
+                width: positionedImage.rect.width - arrangement.margin.f * 2,
+                height: positionedImage.rect.height - arrangement.margin.f * 2)
+            let imageView = UIImageView(frame: imageRect)
+        }
     }
 }
